77f9671b5711ff28ad660d5f6bdde3fdceaba9ba
Rafael Morgado <polimero656@gmail.com>
2022-06-09 22:34:00 +0100

    fixed error on ubuntu

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -26,19 +26,3 @@ const faculties = [
   'icbas'
 ];
 
-const credentials = r'''
-{
-  "type": "service_account",
-  "project_id": "es-2022",
-  "private_key_id": "c3c384e7782beac844febfd743fabc283188b208",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCc86tit1UZh/jc\nyugzFUp0JNUXAb9pHhyp6IWzgwbmI8Z7QuVDFJXCSy+SH2iYFml5Ctu8LZ6nPGDl\nFNnonutPKiOE+AT1/RayXI0Gt+xRLY+uNrnfxz6W9FJqa0EGZBriV83ng4NuRAbb\n1dONisNV5zFnOBVd7A8l3BLid5VmdB1l3TBxSfWSN/L7M1FbeEBpI4JNYD4zToQM\nAPplXUqgZAS7KjK3utg0zb91Mrey0gdwcwrO5F6eTkU1IbzRxiqTF83dZdXg+SUI\nm1rygc+ojdN5xhwgezFNbIyJzM4yX/y83W5pVC6bKEfdzpXJQjdB83AVGq+I1MMc\na52ZXNXZAgMBAAECggEALxMUpfSX5SL3vP1s3Jl3rdM4SIDBMN2TulBf/aIAiTlU\nuDatvctoSZC0cW7TN76NHv1nP48tRTqiU0NIQnGb4TAh3Clpj4A79P42XDwA+K4i\nKJ4IbPp9Jtib+Yg2xhxzRc0RGG3FF2b26au+GndytAq2Mh/LpNJ/Dptt3JgCaupT\neNRuKk8DHCUJnpPlOQKsInCGC+t8EN8u6VIgnoLT221y0XkS+Rh6fom/d7Aaw/Xe\nrJkq9k0oHJ/niuQgxXVtGot28P5qV1A/k9EQgCXbC0ADYabNjXrASXMR1AGn9LyA\n6R8D50i5jIXgoQbCVasIbpym6gpni3i7BL0wjrfZOwKBgQDNmsWt44ezNVJxpeKE\niaQPCThP6gmgGFz0WA3FxU+opbVZUk0I4txuDZirEcteU+5ifNCCPL0suHeqYz1O\ntuAqxskfZKHn4VovEnxbjab+m3a9ts5DAvdkhY6vqUPjMc1BWC99YErML0ftghAd\nRdJTJzqmAuT1Tew5Xpl0z/cD9wKBgQDDbAvY6sw126xx7Lw6f/NKofWzWxS3y9xN\nkZEmqyTDtriDJDRYOeaQ+f4S9N7rjdt9YTExsLmCtmzlWBRf6iXx1koDQaank5Ty\nbl+XSVFruBB3npCHUP5WFjgPqx/66ygAQslKfAwQIMa6FWySuXsGZNNTIuiZoH75\nh9kcL//grwKBgAZ7RJQQwixiF/FBO3zWIfffYIkUZa1IV1XUmMqNR8+XObpRH+CR\n1kUZ5KAv9WtJ1hAAw0n3xDbusccLTdl3ZmWy0pBMydXXgtJNEOKJ+qO4HuwoISR3\nqXjfxPnAqkU1QrU2pHidWn5JV0kjeoKRuumoKO8m1DTveeztEIVgMPdNAoGBALI5\nxgxNB3VPJVS5bWmE6+PzFdw/gwOlHwR3cAPuLhd4e4rO6BZWqBsshDXRscTumXEd\nxPnmjSnTKjl4PQzExVpr7xBNPaqzhDIdy3x0V5eARTRzHqjqiysEFJjlnsL+2Hp0\nbpRJ0c6bteJcS6r9EmBgcALwYWZK48K7zJxSgnDtAoGBAKXlI3FfeSifsZsBZX60\niTbpItDJ3mGzBfjuN4ZXpC6p4oyCwPkcOFxl/o1qjueinF+5Tzg/dU9tc4OZjBrU\nKd3ddVKO9G+Lqi6V4CU3Atz/5Bmi+etWpLFPwilP7VW0DrWol37npdqaq8v9NJNj\niNoGWW1YOiaQfWIgjMBUQv/i\n-----END PRIVATE KEY-----\n",
-  "client_email": "es-2022@es-2022.iam.gserviceaccount.com",
-  "client_id": "116127013941999727617",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/es-2022%40es-2022.iam.gserviceaccount.com"
-}
-''';
-
-const spreadsheetId = '1k-EvQjLzza9qCTD8g6wYCjzJHbQ99emXvAToFjfE2gU';
